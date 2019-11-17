32dbbb25210139d4c11688953ebe4a19a161735d
PlasmaDan <github@plasmadan.com>
2019-11-17 02:30:45 +0000

    Update config.php

--- gui/config.php
+++ gui/config.php
@@ -3,7 +3,7 @@
 /*
  * config.php - CTRL HAT gpio config for web GUI
  * 
- * Copyright (C) 2018 Dan Jones - https://plasmadan.com
+ * Copyright (C) 2019 Dan Jones - https://plasmadan.com
  * 
  * 
  * -----------------------------------------------------------------------------
@@ -19,11 +19,12 @@
  * -----------------------------------------------------------------------------
  */
 
+$dev_mode = false;    // Allows local GUI development
 $pin_base = 100;      // MCP23017 Pin Base
 $i2c_addr = 0x20;     // I2C Address (0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27)
 
 /*
- * Here you can configure each gpio pin mode (in, out, up)
+ * Here you can configure each gpio pin mode (input, output, pullup)
  * http://wiringpi.com/the-gpio-utility/i2c-mcp23008-and-mcp23017-extensions/
  * 
  * NOTE: pin modes are setup on each refresh of the web GUI. This may conflict
@@ -36,29 +37,29 @@ $pin_modes = array(
     1 => 'out',       // GPA1 (Solid State Relay CH1)
     2 => 'out',       // GPA2 (Solid State Relay CH2)
     3 => 'out',       // GPA3 (Solid State Relay CH3)
-    4 => 'out',       // GPA4
-    5 => 'out',       // GPA5
-    6 => 'out',       // GPA6
-    7 => 'out',       // GPA7
+    4 => 'in',        // GPA4
+    5 => 'in',        // GPA5
+    6 => 'in',        // GPA6
+    7 => 'in',        // GPA7
     
     // Bank B
-    8 => 'out',       // GPB0
-    9 => 'out',       // GPB1
-    10 => 'out',      // GPB2
-    11 => 'out',      // GPB3
-    12 => 'out',      // GPB4
-    13 => 'out',      // GPB5
-    14 => 'out',      // GPB6
-    15 => 'out'       // GPB7
+    8 => 'in',        // GPB0
+    9 => 'in',        // GPB1
+    10 => 'in',       // GPB2
+    11 => 'in',       // GPB3
+    12 => 'in',       // GPB4
+    13 => 'in',       // GPB5
+    14 => 'in',       // GPB6
+    15 => 'in'        // GPB7
 );
 
 /*
  * Enable GUI graphic for each Solid State Relay on your CTRL HAT
  */
 
-$ch0_relay = 1;       // Eable CH0 Solid State Relay
-$ch1_relay = 1;       // Eable CH1 Solid State Relay
-$ch2_relay = 1;       // Eable CH2 Solid State Relay
-$ch3_relay = 1;       // Eable CH3 Solid State Relay
+$ch0_relay = true;    // Eable CH0 Solid State Relay
+$ch1_relay = true;    // Eable CH1 Solid State Relay
+$ch2_relay = true;    // Eable CH2 Solid State Relay
+$ch3_relay = true;    // Eable CH3 Solid State Relay
 
 ?>
