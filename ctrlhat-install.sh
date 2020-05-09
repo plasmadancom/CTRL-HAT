984ff3ef2006f78d7cad7aa23308d9be34be78d8
PlasmaDan <github@plasmadan.com>
2020-05-09 02:40:10 +0100

    Update ctrlhat-install.sh

--- ctrlhat-install.sh
+++ ctrlhat-install.sh
@@ -4,6 +4,10 @@
 CONFIG="/boot/config.txt"
 VSFTPD="/etc/vsftpd.conf"
 INPUT="Please answer yes or no."
+GUI="https://github.com/plasmadancom/CTRL-HAT/trunk/gui"
+
+WEBROOT=false
+FTP=false
 
 # Arguments: 1 search, 2 replace, 3 setting name, 4 file
 update_file() {
@@ -11,12 +15,41 @@ update_file() {
 		then
 			echo "Update $3 found in $4 ..."
 			sed -i "/$1/c\\$2" $4
-	
 	else
 		echo "Add $3 in $4 ..."
 		echo "$2" >> $4
 	fi
 }
+
+
+while true; do
+    read -p "Install GUI at web root? This will empty the web root! [Y/n] " yn
+    case $yn in
+        [Yy]* )
+			WEBROOT=true
+			break
+			;;
+        [Nn]* )
+			echo "GUI will be installed in subdirectory: ctrlhat"
+			break
+			;;
+        * ) echo $INPUT;;
+    esac
+done
+
+
+while true; do
+    read -p "Install vsftpd? [Y/n] " yn
+    case $yn in
+        [Yy]* )
+			VSFTPD=true
+			break
+			;;
+        [Nn]* ) break;;
+        * ) echo $INPUT;;
+    esac
+done
+
 
 echo "Enable I2C interface ..."
 raspi-config nonint do_i2c 0
@@ -33,7 +66,8 @@ apt-get upgrade -y
 
 
 echo "Install CTRL HAT required packages ..."
-apt-get install i2c-tools wiringpi apache2 php libapache2-mod-php subversion -y
+apt-get install i2c-tools wiringpi apache2 php libapache2-mod-php subversion python-pip -y
+pip install wiringpi
 
 
 echo "Add pi user to i2c user group ..."
@@ -41,52 +75,34 @@ adduser pi i2c
 
 
 echo "Install CTRL HAT Web GUI ..."
-rm -rf /var/www/html/*
-svn checkout https://github.com/plasmadancom/CTRL-HAT/trunk/gui /var/www/html
+
+# Install at web root?
+if [ "$WEBROOT" = true ]
+	then
+		rm -rf /var/www/html/*
+		svn checkout $GUI /var/www/html
+else
+	mkdir /var/www/html/ctrlhat
+	svn checkout $GUI /var/www/html/ctrlhat
+fi
+
+
+# Install vsftpd?
+if [ "$FTP" = true ]
+	then
+		apt-get install vsftpd -y
+		chown -R pi /var/www
+		update_file "write_enable" "write_enable=YES" "write_enable setting" $VSFTPD
+		update_file "force_dot_files" "force_dot_files=YES" "force_dot_files setting" $VSFTPD
+		service vsftpd restart
+fi
+
+
 chmod -R 755 /var/www
 echo "CTRL HAT Web GUI Installed."
 hostname -I
 
 
-# vsftpd?
-while true; do
-    read -p "Install vsftpd?" yn
-    case $yn in
-        [Yy]* )
-			apt install vsftpd -y
-			chown -R pi /var/www
-			update_file "write_enable" "write_enable=YES" "write_enable setting" VSFTPD
-			update_file "force_dot_files" "force_dot_files=YES" "force_dot_files setting" VSFTPD
-			service vsftpd restart
-			break
-			;;
-        [Nn]* ) break;;
-        * ) echo $INPUT;;
-    esac
-done
+echo "Install Complete. Please reboot."
 
-
-# WiringPi for Python?
-while true; do
-    read -p "Install WiringPi for Python?" yn
-    case $yn in
-        [Yy]* )
-			apt-get install python-pip -y
-			pip install wiringpi
-			break
-			;;
-        [Nn]* ) break;;
-        * ) echo $INPUT;;
-    esac
-done
-
-
-# Reboot?
-while true; do
-    read -p "Install Complete. Reboot?" yn
-    case $yn in
-        [Yy]* ) reboot; break;;
-        [Nn]* ) break;;
-        * ) echo $INPUT;;
-    esac
-done
+exit 0
